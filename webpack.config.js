42736cff021edb83f00c5e513c880ca61ddbe91e
JoachimR <joachim.renard@student.Vinci.be>
2020-11-25 13:43:13 +0100

    refactor - port in webpack.config = 80

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devtool: "cheap-module-eval-source-map",
   devServer: {
     contentBase: path.join(__dirname, "dist"),
-    port: 666,
+    port: 80,
     host: '0.0.0.0', // server to be accessible externally
     public: "localhost", // force to open localhost instead of 0.0.0.0
     open: true, // open the default browser
