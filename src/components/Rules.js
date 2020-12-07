79963bf19a6ec1bb43d35f926c0ed72b36fbac6a
Anthony <anthony.deleener@student.vinci.be>
2020-12-07 13:45:34 +0100

    update regles (orthographe)

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,24 +5,20 @@ var controller = new ScrollMagic.Controller();
 const Rules = () => {
     let rules1;
     rules1=
-    `<div style="height: 50px;"></div> <h1 class="rules" style="text-align: center">Rules</h1><div style="height: 50px;"></div>
+    `<div style="height: 50px;"></div> <h1 class="rules" style="text-align: center">Règles</h1><div style="height: 50px;"></div>
 
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
     <h2>Les bases</h2>
-    <article>Chaque joueur recoit des cartes. Le but est de detecter un symbole commun entre la carte du millieu et celui
-    de votre deck. Une fois trouve, cliquez sur les deux symboles correspendants. Vous vous debarasserez alors d'une de vos
-    cartes. Le premier a se debarasser de toutes ses cartes a gagne.</article>
+    <article>Chaque joueur reçoit des cartes. Le but est de détecter un symbole commun entre la carte du millieu et celle
+    de votre deck. Une fois trouvé, cliquez sur les deux symboles correspondants. Vous vous débarasserez alors d'une de vos
+    cartes. Le premier à se débarasser de toutes ses cartes à gagné.</article>
     </div>
 
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ml-auto" style="text-align: right">
     <h2>Mode solo</h2>
-    <article>Dans le mode solo, le but est de se debarasser de ses cartes le plus rapidement possible. Votre temps sera
-    chronometre, et vous pourrez ensuite vous comparer avec les meilleurs joueurs du site !</article>
+    <article>Dans le mode solo, le but est de se débarasser de ses cartes le plus rapidement possible. Votre temps sera
+    chronometré, et vous pourrez ensuite vous comparer avec les meilleurs joueurs du site !</article>
     </div>
-    
-    
-    
-    
     `;
 
 
