a151d3e5201ce2a5c3c2506fccc881276b5a65a3
Anthony <anthony.deleener@student.vinci.be>
2020-12-13 16:26:48 +0100

    conditions changes

--- src/components/GeneralConditionsPage.js
+++ src/components/GeneralConditionsPage.js
@@ -18,13 +18,12 @@ let generalConditions = `
     <h4>Données personnelles</h4>
     <br>
     <article>
-      Vos données fournies lors de l'inscription sont stockées dans une base de données nous appartenant. Votre nom d'utilisateur
+      Vos données fournies lors de l'inscription sont stockées sur nos serveurs. Votre nom d'utilisateur
       et votre adresse e-mail ne sont pas cryptées et peuvent être accédées par une simple demande en contactant un des développeurs
-      de notre site. Votre mot de passe est crypté dans notre base de données, et son décryptage ne peut pas vous être offert sur une
-      simple demande. Nous nous engageons à ne pas utiliser vos données à des fins commerciales, ni à vous envoyer des e-mails de
-      promotion. La suppression de toutes vos données de notre base de données peut être demandée en envoyant une demande à un des
-      développeurs de notre site. Il sera alors de notre devoir de les supprimer dans les 30 jours suivant votre demande, conformément
-      à la loi européenne sur la vie privée et les données personnelles.
+      de notre site. Votre mot de passe est crypté. Nous nous engageons à ne pas utiliser vos données à des fins commerciales, ni à
+      vous envoyer des e-mails de promotion. La suppression de toutes vos données de nos serveurs peut être demandée en envoyant une
+      demande à un des développeurs de notre site. Il sera alors de notre devoir de les supprimer dans les 30 jours suivant votre
+      demande, conformément à la loi européenne sur la vie privée et les données personnelles.
     </article>
   </div>
 
